6f13a5817a27bf516ca62c8da79ff5a1485a0c47
Spencer Merrill <canadianfaller7@gmail.com>
2023-01-13 00:15:06 -0700

    set up all the models except for index.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,13 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+ try {
+  const categoryData = await Category.findAll({
+    
+  })
+ }
 });
 
 router.get('/:id', (req, res) => {
